344335c13929621f2ec0e2b4e8cc93c0b31b2b4e
cswanstrom <89599865+cswanstrom@users.noreply.github.com>
2021-08-30 22:33:34 -0700

    add toggle for bg color

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function changeClass() {
-    document.body.classList.add('bg-aquamarine');
+    document.body.classList.contains('bg-aquamarine')
+        ? document.body.classList.remove('bg-aquamarine')
+        : document.body.classList.add('bg-aquamarine');
 }
 
 window.onload = function () {
