a98981eb3f27cc3d63fa3eeb8dff3d3167bfa799
cswanstrom <89599865+cswanstrom@users.noreply.github.com>
2021-09-06 15:17:07 -0700

    add canvas demo

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function generate() {
-    document.getElementById("address").placeholder = ":)"
+    document.getElementById("address").style.display = "none";
+    document.getElementById("click-me-button").style.display = "none";
+    var canvas = document.getElementById('drawing');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.canvas.width = 150;
+        ctx.canvas.height = 150;
+
+        ctx.fillStyle = 'aquamarine';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+      }
 }
 
 window.onload = function () {
